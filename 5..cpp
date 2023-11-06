9836fb1ae5467a3c5013b466300f645c121ae373
Ludmila Pačandová <128587728+Lidus1@users.noreply.github.com>
2023-11-06 09:07:57 +0000

    jj

---
+++ 5..cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	int val;
+	Node* next;
+};
+
+void printNode(Node* node);
+Node* newNode(int val);
+Node* addNextNode(int val, Node* node1);
+
+int main()
+{
+
+    Node* node1 = newNode(1);
+    cout<<"Node1:"<<endl;
+    printNode(node1);
+
+    Node* node2 = newNode(2);
+    cout<<endl<<"Node2:"<<endl;
+    printNode(node2);
+
+    node1->next=node2;
+    cout<<endl<<"Node1&2:"<<endl;
+    printNode(node1);
+
+    Node* node3 = addNextNode(3, node1);
+    cout<<endl<<"Node1&2&3:"<<endl;
+    printNode(node1);
+    
+
+
+	return 0;
+	
+}
+
+Node* addNextNode(int val, Node* previous){
+    Node* newNode = new Node();
+    newNode->val;
+    newNode->next = previous->next;
+    previous->next = newNode;
+    return newNode;
+}
+
+void printNode(Node* node)
+{
+	cout << "[Add: " << node << ", val: " << node->val << ", next: " << node->next << "]" << endl;
+	if (node->next != NULL) printNode(node->next);
+}
+
+
+
+Node* newNode(int val)
+{
+	Node* newNode = new Node();
+	newNode->val = val;
+	newNode->next = NULL;
+	return newNode;
+}
