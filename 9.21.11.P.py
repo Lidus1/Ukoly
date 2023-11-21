fb33b522842a6078a7491d79cd2467dc69b1ebb7
Ludmila Pačandová <128587728+Lidus1@users.noreply.github.com>
2023-11-21 10:03:11 +0000

    stromecek

--- 9.21.11.P.py
+++ 9.21.11.P.py
@@ -1,1 +1,23 @@
 
+
+
+
+
+def koruna(vyska:int =5)->None:
+    for i in range (vyska):
+        print(" "*(vyska-i-1),"*"*(2*i+1))
+
+def kmen(vyska_stromu: int = 5,vyska_kmene:int =3, sirka:int =3)->None:
+    for _ in range(vyska_kmene):
+        print(" "*int(vyska_stromu-sirka/2),"#"*sirka)
+
+def test():
+    pass    
+
+koruna()    
+kmen()
+
+
+
+
+
