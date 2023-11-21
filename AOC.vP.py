5d1d9721f5ea19902743a40e41d5e1f765aab2c5
Ludmila Pačandová <128587728+Lidus1@users.noreply.github.com>
2023-11-21 14:14:51 +0000

    AOC 2020

--- AOC.vP.py
+++ AOC.vP.py
@@ -4,11 +4,27 @@ import math
 import time
 
 
-openfile = open("cisla.txt")
+cisla = [int(line.strip()) for line in open("cisla.txt", 'r')]
+print(cisla)
 
-print(openfile.read())
+def soucet(cisla):
+    for i in cisla:
+        for j in cisla:
+            if i + j == 2020:
+                return i, j, i+j, i*j
+            
+                
+print(soucet(cisla))
 
-openfile.close()
+
+
+
+
+
+
+
+
+
 
 
 
