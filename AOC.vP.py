f1bf69a3480585c89f8e57e035b898183a03a662
Ludmila Pačandová <128587728+Lidus1@users.noreply.github.com>
2023-11-21 13:23:24 +0000

    boo

--- AOC.vP.py
+++ AOC.vP.py
@@ -3,7 +3,12 @@
 import math
 import time
 
-cisla = [1,2,3,4,5,6]
-vetsi_cisla = [cislo + 1 for cislo in cisla]
-print (vetsi_cisla)
+
+openfile = open("cisla.txt")
+
+print(openfile.read())
+
+openfile.close()
+
+
 
