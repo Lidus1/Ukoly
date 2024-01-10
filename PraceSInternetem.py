356b1616ce99bdc3d28f038a6e5cae0223f9b4ad
Ludmila Pačandová <128587728+Lidus1@users.noreply.github.com>
2024-01-10 13:54:56 +0000

    ppp

--- PraceSInternetem.py
+++ PraceSInternetem.py
@@ -1,1 +1,10 @@
 
+import requests
+
+url = 'https://www.gutenberg.org/cache/epub/2267/pg2267-images.html'
+r = requests.get(url, allow_redirects=True)
+
+open('PraceSInternetem.html', 'wb').write(r.content)
+
+print('Stazeno')
+
